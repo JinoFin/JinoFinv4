5118cbd3017dfc3a4d192cd5933697a1fd5e3ec7
JinoFin <seifjenane@gmail.com>
2025-09-23 10:29:40 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,8 @@
-// TODO: paste your Firebase config here
-// Example:
-// export const firebaseConfig = {
-//   apiKey: '...',
-//   authDomain: '...',
-//   projectId: '...',
-//   storageBucket: '...',
-//   messagingSenderId: '...',
-//   appId: '...'
-// };
-
-export const firebaseConfig = {}
+export const firebaseConfig = {
+  apiKey: "AIzaSyDuPEWTISexdpvT6k9qF4-NEid1pH5hyWc",
+  authDomain: "jinofin-8f60a.firebaseapp.com",
+  projectId: "jinofin-8f60a",
+  storageBucket: "jinofin-8f60a.firebasestorage.app",
+  messagingSenderId: "788495002884",
+  appId: "1:788495002884:web:99fe93caf91ef32d3bf451"
+};
